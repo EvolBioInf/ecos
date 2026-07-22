51f0b0eb68f5eedbaa34f200d5906bb4ef383c2a
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-07-22 15:40:07 +0200

    Updated intro.sh

--- intro/intro.sh
+++ intro/intro.sh
@@ -2,7 +2,7 @@ taxi -e "Escherichia coli" neidb
 ranks -L complete 562 neidb |
     dot -T svg |
     imv-x11 -
-dree -n -r -L complete -l 562 neidb |
+dree -n -R -L complete -l 562 neidb |
     tail -n +2 |
     sed 's/o /o_/' |
     sort -k 4 -n -r |
